45dee3e315dd7aace95a78831f8e5956b005260c
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2022-01-08 18:49:39 +0100

    huge refactor!: recursive inheritance

--- lua/stl/init.lua
+++ lua/stl/init.lua
@@ -1,96 +1,144 @@
 local M = {}
+local utils = require("stl.utils")
 
-function M.eval(statusline)
-    local stl = {}
-
-    for i, block in ipairs(statusline) do
-        block.index = i
-        table.insert(stl, M.eval_block(block))
-    end
-
-    return table.concat(stl)
-end
-
-function M.eval_block(block)
-    local stl = {}
-    block.hl = block.hl or {} -- function or table
-
-    if block.init then block:init() end
-
-    for i, component in ipairs(block) do
-        component.index = i
-        component.block = block
-        table.insert(stl, M.eval_component(component))
-    end
-
-    return table.concat(stl)
-end
-
-function M.eval_component(component)
-    component.hl = component.hl or {} -- function or table
-    component.condition = component.condition or component.block.condition or function() return true end
-
-    if component.init then component:init() end
-
-    if not component:condition() then return "" end
-
-    local provider_str = type(component.provider) == "function" and (component:provider() or "") or (component.provider or "") -- string or ""
-
-    local component_hl = type(component.hl) == "function" and component:hl() or component.hl -- table
-    local block_hl = type(component.block.hl) == "function" and component.block:hl() or component.block.hl -- table
-    local hl = vim.tbl_extend("force", block_hl, component_hl) -- table
-
-    local hl_str_start, hl_str_end = M.eval_hl(hl)
-
-    return hl_str_start .. provider_str .. hl_str_end
-end
+M.defined_highlights = {}
+M.statusline = {}
 
 function M.eval_hl(hl)
     if vim.tbl_isempty(hl) then
         return "", ""
     else
+        hl.name = hl.name
+            or (
+                "Stl"
+                .. (hl.fg and hl.fg:gsub("#", "_") or "")
+                .. (hl.bg and hl.bg:gsub("#", "_") or "")
+                .. (hl.style and hl.style:gsub(",", "_") or "")
+                .. (hl.guisp and hl.guisp:gsub(",", "_") or "")
+            )
         if not M.defined_highlights[hl.name] then
             M.make_hl(hl)
         end
-        return "%#" .. hl.name .. "#", "%*"
+        return "%#" .. hl.name .. "#", "" --"%*"
     end
 end
 
-M.defined_highlights = {}
+-- function M.eval_hl(hl)
+--     if vim.tbl_isempty(hl) then
+--         return "", ""
+--     else
+--         M.make_hl(hl)
+--         return "%#" .. hl.name .. "#", "" --"%*"
+--     end
+-- end
 
 function M.make_hl(hl)
-    local name = hl.name
     if hl.link then
-        vim.cmd("highlight! link " .. name .. " " .. hl.link)
+        vim.cmd("highlight! link " .. hl.name .. " " .. hl.link)
     else
         local fg = hl.fg and "guifg=" .. hl.fg .. " " or ""
         local bg = hl.bg and "guibg=" .. hl.bg .. " " or ""
         local style = hl.style and "gui=" .. hl.style .. " " or ""
         local guisp = hl.guisp and "guisp=" .. hl.guisp .. " " or ""
         if fg or bg or style or guisp then
-            vim.cmd("highlight " .. name .. " " .. fg .. bg .. style .. guisp)
+            vim.cmd("highlight " .. hl.name .. " " .. fg .. bg .. style .. guisp)
         end
     end
-    table.insert(M.defined_highlights, hl.name)
+    M.defined_highlights[hl.name] = true
 end
+
+--
+-- function M.statusline()
+--     for i, statusline in ipairs(M.statuslines) do
+--         statusline.index = i
+--         if statusline:condition() then
+--             return M.eval(statusline)
+--         end
+--     end
+-- end
 
 function M.load()
-    vim.cmd("set statusline=%{%v:lua.require'stl'.statusline()%}")
+    vim.cmd("set statusline=%{%v:lua.require'stl'.eval()%}")
 end
 
-M.statuslines = {}
-
 function M.setup(config)
-    M.statuslines = config.statuslines
+    M.statusline = M.StatusLine:new(config.statuslines)
     M.load()
 end
 
-function M.statusline()
-    for _, statusline in ipairs(M.statuslines) do
-        if statusline:condition() then
-            return M.eval(statusline)
+function M.eval()
+    return M.statusline:eval()
+end
+
+local StatusLine = {
+    -- hl = { fg = utils.get_highlight("StatusLine").fg, bg = utils.get_highlight("StatusLine").bg, name = "StlDefault" },
+    hl = { fg = utils.get_highlight("StatusLine").fg, bg = utils.get_highlight("StatusLine").bg },
+    cur_hl = {},
+}
+
+function StatusLine:new(child)
+    child = child or {}
+    local new = {}
+    new.hl = child.hl -- TODO: table highlights should be merged here and assigned their name
+    new.condition = child.condition
+    new.init = child.init
+    new.provider = child.provider
+    new.stop = child.stop
+    setmetatable(new, self)
+    self.__index = function(t, v)
+        if v == "stop" then
+            return nil
+        else
+            return self[v]
         end
     end
+    for i, sub in ipairs(child) do
+        new[i] = new:new(sub)
+    end
+    return new
 end
+
+-- function StatusLine:new(child)
+--     child = child or {}
+--     setmetatable(child, self)
+--     self.__index = self
+--     for i, sub in ipairs(child) do
+--         child:new(sub)
+--     end
+--     return child
+-- end
+
+function StatusLine:eval()
+    local stl = {}
+
+    if self.condition and not self:condition() then
+        return ""
+    end
+
+    if self.init then
+        self:init()
+    end
+
+    local hl = type(self.hl) == "function" and self:hl() or self.hl -- table
+    self.cur_hl = vim.tbl_extend("force", self.cur_hl, hl) -- table
+
+    if self.provider then
+        local provider_str = type(self.provider) == "function" and (self:provider() or "") or (self.provider or "")
+        local hl_str_start, hl_str_end = M.eval_hl(self.cur_hl)
+        table.insert(stl, hl_str_start .. provider_str .. hl_str_end)
+    end
+
+    for i, child in ipairs(self) do
+        local out = child:eval()
+        table.insert(stl, out)
+        if self.stop and out ~= "" then
+            break
+        end
+    end
+
+    return table.concat(stl, "")
+end
+
+M.StatusLine = StatusLine
 
 return M
